24a9c55e94bf58c28974f85f7570b3d57c35e66b
CaptainJS <45124354+CaptainJavaScript@users.noreply.github.com>
2018-12-27 17:58:17 +0100

    .

--- Seaman-Client.js
+++ Seaman-Client.js
@@ -1,10 +1,17 @@
 const ContractConnector = require("./Seaman-Lib.js");
 
-ContractConnector.Invoke_ActivateVoucher_AtSeamanMAINNET();
-ContractConnector.Invoke_CallbackExample_AtSeamanMAINNET();
-ContractConnector.Invoke_CentimeterToInchExample_AtSeamanMAINNET("19");
-ContractConnector.Invoke_WolframAlphaExample_AtSeamanMAINNET("France");
+var IsMainnet = false;
 
-ContractConnector.ListenToSeamanMAINNET((element) => {
+//ContractConnector.Invoke_ActivateVoucher_AtSeaman(IsMainnet);
+//ContractConnector.Invoke_CentimeterToInchExample_AtSeaman(IsMainnet, "14");
+//ContractConnector.Invoke_WolframAlphaExample_AtSeaman(IsMainnet, "Spain");
+//ContractConnector.Invoke_CallbackExample_AtSeaman(IsMainnet);
+
+
+ContractConnector.ListenToSeaman(IsMainnet, (element) => {
     console.log("--> SEMAN [" + element.blockNumber + "] " + element.event + ": " + element.returnValues[0]);
-});
+},
+(error) => { 
+    console.log(error); 
+},
+(log) => console.log);
